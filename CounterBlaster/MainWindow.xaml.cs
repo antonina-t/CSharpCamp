d8f7a648764719b783130fd0c24ea1f59ee37d4c
antonina_n <antonina.nevstedt@consid.se>
2019-08-17 23:48:36 +0200

    Add CounterBlaster

---
+++ CounterBlaster/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace CounterBlaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int clicks = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clicks++;
+            label.Content = "Clicks: " + clicks;
+            label.FontSize++;
+        }
+    }
+}
